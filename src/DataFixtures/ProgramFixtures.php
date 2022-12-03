66339b8fdde1bffac366863752ab33bb211b56b4
seb papet <papet.seb@gmail.com>
2022-12-03 17:58:02 +0100

    use refercence existente queste 9

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $program = new Program();
+        $program->setTitle('Walking dead');
+        $program->setSynopsis('Des zombies envahissent la terre');
+        $program->setCategory($this->getReference('category_Action'));
+        $manager->persist($program);
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
