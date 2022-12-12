11832458a1a99a63b81a5a0cc887754e48abd335
seb papet <papet.seb@gmail.com>
2022-12-12 15:40:02 +0100

    modif show et categorycontroller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,17 +14,26 @@ class CategoryController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        $categorys = $categoryRepository->findALl();
+        $categorys = $categoryRepository->findAll();
         return $this->render('category/index.html.twig', [
             'categorys' => $categorys,
         ]);
     }
-    #[Route('/show/{id}', methods: ['GET'], name: 'show')]
-    public function show(string $categoryName)
+    #[Route('/show/{categoryName}', methods: ['GET'], name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
-        $category = $categoryName->findOneBy([]);
+        $category = $categoryRepository->findBy(['name' => $categoryName]);
+        var_dump($category);
+
+        $categoryId = $category[0]->getId();
+        var_dump($categoryId);
+
+        $programs = $programRepository->findBy(['category' => $categoryId], ['id' => 'DESC'], 3);
+        var_dump($programs);
+
         return $this->render('category/show.html.twig', [
             'category' => $category,
+            'programs' => $programs
         ]);
     }
 }
